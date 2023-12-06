f14df6520bffb1f0fd2e5fa4025589579cac2d31
newgate21 <124586622+newgate21@users.noreply.github.com>
2023-12-06 17:33:48 +0100

    added collision and bounce management

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,11 +6,43 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private float ProjectileSpeed = 20;
 
+    private Vector2 velocity;
     private Rigidbody2D rigidBody;
+    private CollisionDetector collisionDetector;
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        if (collisionDetector = GetComponent<CollisionDetector>())
+        {
+            // Subscribe to the collision event
+            collisionDetector.OnCollisionEnterEvent += OnCollisionHandler;
+        }
+    }
+
+    private void Update()
+    {
+        velocity = rigidBody.velocity;
+    }
+
+    void OnCollisionHandler(Collision2D _collision)
+    {
+        if (_collision.gameObject.CompareTag("Wall") || 
+            _collision.gameObject.CompareTag("Block"))
+        {
+            Bounce(_collision);
+        }
+    }
+
+    public void Bounce(Collision2D _collision)
+    {
+        // Check if the projectile has a Rigidbody component
+        if (rigidBody == null) return;
+
+        if (_collision.contactCount <= 0) return;
+
+        // Apply the reflected velocity to the object
+        rigidBody.velocity = Vector2.Reflect(velocity, _collision.GetContact(0).normal);
     }
 
     public void Fire(Vector2 _direction)
@@ -20,5 +52,11 @@ public class Projectile : MonoBehaviour
 
         // Apply force to shoot the projectile
         rigidBody.AddForce(_direction * ProjectileSpeed, ForceMode2D.Impulse);
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from the collision event
+        collisionDetector.OnCollisionEnterEvent -= OnCollisionHandler;
     }
 }
